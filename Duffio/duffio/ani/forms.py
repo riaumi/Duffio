5bf79a2cdb6bee3af5519d2bbdd4066b04eeaf3a
rika.suzuki <rika.suzuki0807@gmail.com>
2022-08-03 00:00:19 +0900

    一覧、追加を作成する

--- Duffio/duffio/ani/forms.py
+++ Duffio/duffio/ani/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from .models import Day
 
 class DayCreateForm(forms.ModelForm):
-
     class Meta:
         model = Day
         fields='__all__'
--- Duffio/duffio/ani/views.py
+++ Duffio/duffio/ani/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .forms import DayCreateForm
+from .models import Day
 
 def top(request):
     return render(request,'ani/top.html')
@@ -8,6 +9,9 @@ def profile(request):
     return render(request,'ani/profile.html')
 
 def diary(request):
+    context = {
+        'diary':Day.objects.all(),
+    }
     return render(request,'ani/diary.html')
 
 def add(request):
@@ -15,7 +19,7 @@ def add(request):
     form = DayCreateForm(request.POST or None)
 
     # method=POST、つまり送信ボタンを押下時、入力内容に問題がなければ
-    if request.method == 'POST' and form.is_valid():
+    if request.method =="POST" and form.is_valid():
         form.save()
         return redirect('ani:diary')
     
